ccf3dec37b024473c91ce8a1f802b018b097dc2a
FranzGuanaco <chevin.pierre.tomas@gmail.com>
2022-10-01 18:28:18 +0200

    premier brouillon

--- Interface_back.php
+++ Interface_back.php
@@ -3,7 +3,8 @@
 $mysqlDsn = 'mysql: host=localhost; dbname=dbClient';
 
 
-$DBClient = "CREATE DATABASE dbClient";
+
+/*$DBClient = "CREATE DATABASE dbClient";
 $tableClient = "CREATE TABLE Clients_data
 ( Id int not null auto_increment,
     name varchar (50) not null,
@@ -11,18 +12,41 @@ $tableClient = "CREATE TABLE Clients_data
     url varchar (50) not null,
     primary key (Id)
 
-)";
+)"; */
+
+/* $data = [
+    ['Jean Frederic', 'manager', 'root'],
+    [ 'Marc Ruffier', 'technician', 'root'],
+    [ 'Stephanie Dublanc', 'technician', 'root'],
+    [ 'Ashley Fiss', 'technician', 'root'], 
+    [ 'Frank Lin', 'technician', 'root'],
+    [ 'Bastien Cedric', 'technician', 'root'],
+    [ 'Jean Noire', 'technician', 'root'],
+    [ 'Fiona Desley', 'technician', 'root'],
+    [ 'Pierre Moulin', 'technician', 'root'], ]; */
+
+
     
+
+
 
 try {
+
     $pdo = new PDO($mysqlDsn, $db_password ='root', $db_user ='root');
+
+   /*  $stmt = $pdo->prepare("INSERT INTO Clients_data (name, description, url) VALUES (?,?,?)");
+
+    $pdo->beginTransaction();
+    foreach ($data as $row)
+    {
+        $stmt->execute($row);
+    }
+    $pdo->commit(); */
+
+    
 
 
     
-    if ($pdo -> query($tableClient)){
-
-      echo "Connected successfully";
-    }
     
 
    
